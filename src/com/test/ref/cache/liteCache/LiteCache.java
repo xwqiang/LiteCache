ceeee4d336b2d05f126ec10a482064eee32db76d
xwqiang <642143714@qq.com>
2013-12-19 16:48:54 +0800

    init

---
+++ src/com/test/ref/cache/liteCache/LiteCache.java
@@ -0,0 +1,48 @@
+package com.test.ref.cache.liteCache;
+
+import java.lang.ref.ReferenceQueue;
+import java.util.Hashtable;
+
+public class LiteCache<T> {
+	private Hashtable<String, CacheRef<T>> refs;// 用于Chche内容的存储
+	private String key;
+	private ReferenceQueue<T> queue;// 垃圾Reference的队列
+	public LiteCache(){
+		this.refs = new Hashtable<String,CacheRef<T>>();
+		this.queue = new ReferenceQueue<T>();
+	}
+	public int size(){
+		return refs.size();
+	}
+	
+	public void setKey(String key){
+		this.key = key;
+	}
+	public void put(T t){
+		cleanCache();
+		CacheRef<T> cacheRef = new CacheRef<T>(t,queue,key);
+		refs.put(cacheRef.getKey(), cacheRef);
+	}
+	@SuppressWarnings("unchecked")
+	private void cleanCache(){
+		
+		CacheRef<T> obj = null;
+		
+		while ((obj = (CacheRef<T>) queue.poll()) != null) {
+			refs.remove(obj.getKey());
+		}
+	}
+	
+	public void clearCache() {
+		cleanCache();
+		refs.clear();
+		System.gc();
+		System.runFinalization();
+	}
+	public T get(String key){
+		if(refs.get(key)==null){
+			return null;
+		}
+		return refs.get(key).get();
+	}
+}
---
+++ src/com/test/ref/cache/liteCache/Test.java
@@ -0,0 +1,21 @@
+package com.test.ref.cache.liteCache;
+
+
+public class Test {
+
+public static void main(String[] args ){
+		LiteCache<UserInfo> c = new LiteCache<UserInfo>();
+		c.setKey("user_id");
+		try {
+			for(int i = 0 ;i<1000000000;i++){
+				UserInfo u = new UserInfo();
+				u.setUser_id("user_"+i);
+				c.put(u);
+				System.out.println(c.size());
+			} 
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		System.out.println("over");
+	}
+}
